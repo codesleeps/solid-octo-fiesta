d26be8c29d093ff2e9303b64be21522f3b6ab512
Codesleep <125808990+codesleeps@users.noreply.github.com>
2023-05-10 14:45:26 +0100

    Add files via upload

---
+++ ReadMe.md
@@ -0,0 +1,79 @@
+
+# WELCOME TO MY BOOTCAMP ASSIGNMENT REPO
+
+# Hi!... I'm Code Sleep
+
+## About Me
+
+I'm a front end developer...
+
+[![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://codesleeps.github.io/Bootstrap-Portfolio/)
+
+![logo001](https://user-images.githubusercontent.com/125808990/236808958-deddef64-0c3e-4e5b-92ce-84166aa87fc6.png)
+
+🦾 I'm currently working on...React UI
+
+🧠 I'm currently learning..._Python_ _MongoDB_ _PHP_ _Next.JS_
+
+💵 I'm looking to collaborate on...any A.I. Projects
+
+🤔 I'm looking for help with...github actions
+
+💬 Ask me about...Travel, manufacturing or design
+
+📫 How to reach me...links below
+
+⚡️ Fun fact...keep smiling 🤖
+
+![Visitor Count](https://profile-counter.glitch.me/codesleeps/count.svg)
+
+# Title
+
+  README GENERATOR
+  
+## License
+
+  MIT
+
+## Username
+
+  CODESLEEP
+
+### Logo
+
+  '/Users/codesleep/Desktop/readmeGenerator/assets/IMAGES/logo.png'
+
+## Email
+
+  <codesleep43@gmail.com>
+
+## Description
+
+  To be able to generate a readme file from user input
+
+## Usage
+
+  daily or else
+
+## Installation
+
+  npm install
+
+## Learn
+
+  automate the boring stuff
+
+## Test
+
+  none
+
+## Contributing
+
+  none
+
+## Additional Resources
+
+   <https://www.youtube.com/watch?v=v2GKt39-LPA>, <https://github.com/codesleeps/jokesGenerator/blob/main/README.md>
+
+  THANK YOU FOR READING THIS REPO PLEASE VISIT AGAIN FOR MY LATEST PROJECTS!
+  
---
+++ index.js
@@ -0,0 +1,84 @@
+// npm install --save inquirer@^8.0.0
+// variables for dependancies
+let inquirer = require('inquirer');
+let fs = require('fs');
+let readmeGenerator = require('./assets/readmeGenerator') ;
+
+
+// questions
+let questions = [
+  {
+    type: 'input',
+    message: 'What is the title of your project?',
+    name: 'title'
+  },
+  {
+    type: 'list',
+    message: 'What license would you like to use?',
+    name: 'license',
+    choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+  },
+  {
+    type: 'input',
+    message: 'What is your GitHub username?',
+    name: 'username'
+  },
+  {
+    type: 'input',
+    message: 'Do you have a logo?',
+    name: 'logo',
+  },
+  {
+    type: 'input',
+    message: 'What is your email address?',
+    name: 'email'
+  },
+  {
+    type: 'input',
+    message: 'What is the description of your project?',
+    name: 'description'
+  },
+  {
+    type: 'input',
+    message: 'What are the usage information?',
+    name: 'usage'
+  },
+  {
+    type: 'input',
+    message: 'What are the installation instructions?',
+    name: 'installation'
+  },
+  {
+    type: 'input',
+    message: 'What did you learn building this project?',
+    name: 'learn'
+  },
+  {
+    type: 'input',
+    message: 'What are the test instructions?',
+    name: 'test',
+  },
+  {
+    type: 'input',
+    message: 'What are the contribution guidelines?',
+    name: 'contributing',
+  },
+  {
+    type: 'input',
+    message: 'What additional resources did you use?',
+    name: 'resources',
+  }
+];
+
+
+// function to write readme 
+inquirer.prompt(questions).then(function(response) {
+  console.log(response);
+  
+   let content = readmeGenerator(response);
+   console.log(content);
+    fs.writeFile("./ReadMe.md", content, function(err){
+        if (err) throw err
+        console.log("Success Job Done!");
+    });
+} ); 
